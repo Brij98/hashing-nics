46589a6ccc9c79d6d484dc1bccfb953b76bb029c
Brijesh Prajapati <brij.p98@gmail.com>
2022-05-04 16:33:31 -0700

    passed all the tests 19/19

--- SensorCluster.cpp
+++ SensorCluster.cpp
@@ -14,6 +14,8 @@ using std::endl;
 unsigned int hexa_to_number(char ch) {
     unsigned int temp = 0;
     // TODO: implement this function
+    if(ch >= '0' && ch <= '9') temp = ch - '0';
+    if (ch >= 'A' && ch <= 'F') temp = ch - 'A' + 10;
     return temp;
 }
     
@@ -21,37 +23,38 @@ unsigned int hexa_to_number(char ch) {
 // function to return the hash value based on the first digit
 unsigned int hashfct1(string nic) {
   // TODO: implement this function, then delete the return statement below
-    return 0;  
+  // std::cout << "hashfct1 output: " + hexa_to_number(nic[0]) << endl;  // debug
+  return hexa_to_number(nic[0]);
 }
 
 // function to return the hash value based on the second digit
 unsigned int hashfct2(string nic) {
    // TODO: implement this function, then delete the return statement below
-    return 0; 
+    return hexa_to_number(nic[1]);
 }
 
 // function to return the hash value based on the third digit
 unsigned int hashfct3(string nic) {
    // TODO: implement this function, then delete the return statement below
-	return 0; 
+	return hexa_to_number(nic[2]);
 }
 
 // function to return the hash value based on the fourth digit
 unsigned int hashfct4(string nic) {
    // TODO: implement this function, then delete the return statement below
-	return 0; 
+	return hexa_to_number(nic[3]);
 }
 
 // function to return the hash value based on the fifth digit
 unsigned int hashfct5(string nic) {
     // TODO: implement this function, then delete the return statement below
-	return 0; 
+	return hexa_to_number(nic[4]);
 }
 
 // function to return the hash value based on the fourth digit
 unsigned int hashfct6(string nic) {
     // TODO: implement this function, then delete the return statement below
-	return 0; 
+	return hexa_to_number(nic[5]);
 }
 
 // Constructor for struct Item
@@ -80,15 +83,28 @@ void SensorNIC::readTextfile(string filename) {
 // function that adds the specified NIC to the sensor network (i.e., to all hash tables)
 void SensorNIC::addItem(string itemName, string nic) {
   // TO DO: implement this function, then delete the return statement below
-   return;
+  struct Item itemToAdd = {itemName, nic};
+  hT1.insert({nic, itemToAdd});
+  hT2.insert({nic, itemToAdd});
+  hT3.insert({nic, itemToAdd});
+  hT4.insert({nic, itemToAdd});
+  hT5.insert({nic, itemToAdd});
+  hT6.insert({nic, itemToAdd});
+  // return;
 }
 
 // function that removes the sensor specified by the nic value from the network
 // if sensor is found, then it is removed and the function returns true
 // else returns false
 bool SensorNIC::removeItem(string nic) {
-   // TODO: implement this function, then delete the return statement below   
-    return false;
+  // TODO: implement this function, then delete the return statement below
+  hT1.erase(nic);
+  hT2.erase(nic);
+  hT3.erase(nic);
+  hT4.erase(nic);
+  hT5.erase(nic);
+  hT6.erase(nic);
+  return true;
 }
 
 // function that decides the best hash function, i.e. the ones among
@@ -99,22 +115,78 @@ bool SensorNIC::removeItem(string nic) {
 // Calculate the balance of each hashtable, one by one.
 unsigned int SensorNIC::bestHashing() {
   // TODO: implement this function, then delete the return statement below
-    
-  
-	/*
-	 for (unsigned i=0; i<10; ++i) {
-    cout << hT1.bucket_size(i); // Call bucket_size() to get the size of each bucket
+  unsigned int arr[6];
+
+  // Hash table 1
+  unsigned int low = hT1.bucket_size(0);
+  unsigned int high = hT1.bucket_size(0);
+  for (unsigned int i=0; i<16; ++i) {
+    if (hT1.bucket_size(i) > high) high = hT1.bucket_size(i);
+    if (hT1.bucket_size(i) < low) low = hT1.bucket_size(i);
+    // cout << hT1.bucket_size(i); // Call bucket_size() to get the size of each bucket
   }
-	*/
+  arr[0] = high - low;
+	
+  low = hT2.bucket_size(0);
+  high = hT2.bucket_size(0);
+  for (unsigned int i=0; i<16; ++i) {
+    if (hT2.bucket_size(i) > high) high = hT2.bucket_size(i);
+    if (hT2.bucket_size(i) < low) low = hT2.bucket_size(i);
+    // cout << hT1.bucket_size(i); // Call bucket_size() to get the size of each bucket
+  }
+  arr[1] = high - low;
+
+  low = hT3.bucket_size(0);
+  high = hT3.bucket_size(0);
+  for (unsigned int i=0; i<16; ++i) {
+    if (hT3.bucket_size(i) > high) high = hT3.bucket_size(i);
+    if (hT3.bucket_size(i) < low) low = hT3.bucket_size(i);
+  }
+  arr[2] = high - low;
+
+  low = hT4.bucket_size(0);
+  high = hT4.bucket_size(0);
+  for (unsigned int i=0; i<16; ++i) {
+    if (hT4.bucket_size(i) > high) high = hT4.bucket_size(i);
+    if (hT4.bucket_size(i) < low) low = hT4.bucket_size(i);
+  }
+  arr[3] = high - low;
+
+  low = hT5.bucket_size(0);
+  high = hT5.bucket_size(0);
+  for (unsigned int i=0; i<16; ++i) {
+    if (hT5.bucket_size(i) > high) high = hT5.bucket_size(i);
+    if (hT5.bucket_size(i) < low) low = hT5.bucket_size(i);
+  }
+  arr[4] = high - low;
+
+  low = hT6.bucket_size(0);
+  high = hT6.bucket_size(0);
+  for (unsigned int i=0; i<16; ++i) {
+    if (hT6.bucket_size(i) > high) high = hT6.bucket_size(i);
+    if (hT6.bucket_size(i) < low) low = hT6.bucket_size(i);
+  }
+  arr[5] = high - low;
+
 	// Then, calculate the lowest balance
     
-    return 100;
+  unsigned int bestHashTblIndx = 0;
+  unsigned int bestHashTblVal = arr[0];
+
+  for(unsigned int i = 0; i < 6; i++){
+    if(arr[i] < bestHashTblVal){
+      bestHashTblVal = arr[i]; 
+      bestHashTblIndx = i;
+    } 
+  }
+
+    return bestHashTblIndx + 1;
 }
 
 // ALREADY COMPLETED
 size_t SensorNIC::size() {
-    if ((hT1.size() != hT2.size()) || (hT1.size() != hT3.size()) || (hT1.size() != hT4.size()) || (hT1.size() != hT5.size())|| (hT1.size() != hT6.size()) )
-  	throw std::length_error("Hash table sizes are not the same");
+    if ((hT1.size() != hT2.size()) || (hT1.size() != hT3.size()) || (hT1.size() != hT4.size()) || (hT1.size() != hT5.size()) || (hT1.size() != hT6.size()) )
+  	  throw std::length_error("Hash table sizes are not the same");
     
 	return hT1.size();
 }
